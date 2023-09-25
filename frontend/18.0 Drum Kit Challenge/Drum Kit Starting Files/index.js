75d2bdd7e4be633d96eeeaace5a066c6456bbf62
Ibrahim san <75019839+ibra-san@users.noreply.github.com>
2023-09-25 22:14:49 +0400

    added sound left button click

--- frontend/18.0 Drum Kit Challenge/Drum Kit Starting Files/index.js
+++ frontend/18.0 Drum Kit Challenge/Drum Kit Starting Files/index.js
@@ -1,20 +1,49 @@
-const allDrums = document.querySelectorAll(".drum") // this is an array of all the elements that contain drum as its class
- 
+const allDrums = document.querySelectorAll(".drum") 
+const tom1Sound = new Audio("./sounds/tom-1.mp3");
+const tom2Sound = new Audio("./sounds/tom-2.mp3");
+const tom3Sound = new Audio("./sounds/tom-3.mp3");
+const tom4Sound = new Audio("./sounds/tom-4.mp3");
+const crashSound = new Audio("./sounds/crash.mp3");
+const kickBassSound = new Audio("./sounds/kick-bass.mp3");
+const snareSound = new Audio("./sounds/snare.mp3");
+
 
 for (let i=0; i<= allDrums.length; i++) { 
     if(allDrums[i].textContent === "w") { 
-        allDrums[i].style.backgroundImage = "url('./images/tom1.png')"
+        allDrums[i].style.backgroundImage = 'url("./images/tom1.png")'
+        allDrums[i].addEventListener("click", () => { 
+            tom1Sound.play();
+        })
     }else if(allDrums[i].textContent === "a") { 
         allDrums[i].style.backgroundImage = "url('./images/tom2.png')"
+        allDrums[i].addEventListener("click", () => { 
+            tom2Sound.play();
+        })
     }else if(allDrums[i].textContent === "s") { 
         allDrums[i].style.backgroundImage = "url('./images/tom3.png')"
+        allDrums[i].addEventListener("click", () => { 
+            tom3Sound.play();
+        })
     }else if(allDrums[i].textContent === "d") { 
         allDrums[i].style.backgroundImage = "url('./images/tom4.png')"
+        allDrums[i].addEventListener("click", () => { 
+            tom4Sound.play();
+        })
     }else if(allDrums[i].textContent === "j") { 
         allDrums[i].style.backgroundImage = "url('./images/snare.png')"
+        allDrums[i].addEventListener("click", () => { 
+            snareSound.play();
+        })
     }else if(allDrums[i].textContent === "k") { 
         allDrums[i].style.backgroundImage = "url('./images/crash.png')"
+        allDrums[i].addEventListener("click", () => { 
+            crashSound.play();
+        })
     }else if(allDrums[i].textContent === "l") { 
         allDrums[i].style.backgroundImage = "url('./images/kick.png')"
+        allDrums[i].addEventListener("click", () => { 
+            kickBassSound.play();
+        })
     }
 }
+
