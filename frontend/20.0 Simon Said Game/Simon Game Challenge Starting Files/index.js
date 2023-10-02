fdbb4414b557db81edea1496543547e055648b33
Ibrahim san <75019839+ibra-san@users.noreply.github.com>
2023-10-02 17:45:33 +0400

    update 3

--- frontend/20.0 Simon Said Game/Simon Game Challenge Starting Files/index.js
+++ frontend/20.0 Simon Said Game/Simon Game Challenge Starting Files/index.js
@@ -19,34 +19,45 @@ $(window).on("load", function () {
     let patternNum = Math.floor(((Math.random())*4)+1);
     let level = 1; 
     patternArray[0] = patternNum;
-
+    let runonce = 1;
     // setting the randoms numbers to btns
     
 
 
     $(window).on("keypress", function () { 
-        $("#level-title").text(`Level ${level}`);
-        // Run this once after the first press
-        switch(patternNum) {
-            case 1: {
-                $("#green").fadeOut(500); 
-                $("#green").fadeIn(500); 
-                console.log("Green")
-                break;
-            } case 2: { 
-                $("#red").fadeOut(500); 
-                $("#red").fadeIn(500); 
-                break
-            } case 3: { 
-                $("#yellow").fadeOut(500); 
-                $("#yellow").fadeIn(500); 
-                break
-            } case 4: { 
-                $("#blue").fadeOut(500); 
-                $("#blue").fadeIn(500); 
-                break
+
+        while (runonce >= 1) {
+            $("#level-title").text(`Level ${level}`);
+            // Run this once after the first press
+            switch(patternNum) {
+                case 1: {
+                    $("#green").fadeOut(500); 
+                    $("#green").fadeIn(500); 
+                    console.log("Green")
+                    break;
+                } case 2: { 
+                    $("#red").fadeOut(500); 
+                    $("#red").fadeIn(500); 
+                    break
+                } case 3: { 
+                    $("#yellow").fadeOut(500); 
+                    $("#yellow").fadeIn(500); 
+                    break
+                } case 4: { 
+                    $("#blue").fadeOut(500); 
+                    $("#blue").fadeIn(500); 
+                    break
+                }
             }
+
+            runonce--;
         }
+        
     })
 
 })
+
+
+function play () { 
+    
+}
