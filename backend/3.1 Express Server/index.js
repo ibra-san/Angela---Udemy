d3cd168667e7bb414b08d10dc727271e480dafd8
Ibrahim san <75019839+ibra-san@users.noreply.github.com>
2023-12-02 20:52:22 +0400

    http request class done

--- backend/3.1 Express Server/index.js
+++ backend/3.1 Express Server/index.js
@@ -5,7 +5,15 @@ const port = 3000;
 // lesson 203 - HTTP Request
 
 app.get("/", (req, res) => { 
-    res.send("Hello, World!");
+    res.send("<h1> Hello from Dubai </h1>");
+})
+
+app.get("/content", (req, res) => { 
+    res.send("<h2> This is the Dubai Content Page </h2>")
+})
+
+app.get("/about", (req, res) => { 
+    res.send("<h3> This is the Dubai about Page </h3>")
 })
 
 app.listen(port, () => { 
